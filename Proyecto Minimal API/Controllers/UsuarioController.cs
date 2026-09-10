61c19c1616a3e2034d7b7554d2b5c487aceb6eeb
Pekac <titi.rojas09@gmail.com>
2026-09-10 11:01:41 -0300

    Rename carpeta, definicion constructor controller

---
+++ Proyecto Minimal API/Controllers/UsuarioController.cs
@@ -0,0 +1,19 @@
+using System;
+using Models;
+using Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly UsuarioService _usuarioService;
+
+        public UsuarioController(UsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+    }
+}
